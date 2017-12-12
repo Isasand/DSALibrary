d584ff079b00b0b00a54b14c7183a631c54d9218
isa sand <isasandh@gmail.com>
2017-12-12 14:26:12 +0100

    stack implemented

--- stack.py
+++ stack.py
@@ -5,3 +5,21 @@ Created on Mon Dec 11 10:52:26 2017
 @author: Isa
 """
 
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self): 
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+        
+    def pop(self):
+        return self.items.pop() 
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
